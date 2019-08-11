c2204336cd8938a6ea7fbb5fa54fb6bdb6a07074
Krishna Manchikalapudi <krishna.manchikalapudi@gmail.com>
2019-08-10 23:29:39 -0700

    clean up jars

--- wiremock/README.md
+++ wiremock/README.md
@@ -1,12 +1,19 @@
 ## Start Mock service
 * execute start.sh or restart.sh
 
+## [optional] windows folks
+* Download jar from http://repo1.maven.org/maven2/com/github/tomakehurst/wiremock-jre8-standalone/2.24.1/wiremock-jre8-standalone-2.24.1.jar
+
 ## Mock service data setup
 * Create API name json file at folder 'mappings'. example: mappings/user.json
 * API resource in the one file. example: mappings/user.json contains 2 resources: /v1/user/health, /user/v1/
 * __file/user/v1/health.json contains the response body
 
 ## Test service
+curl -O http://repo1.maven.org/maven2/com/github/tomakehurst/wiremock-jre8-standalone/2.24.1/wiremock-jre8-standalone-2.24.1.jar  
+
+
+
 - Health test
 ````
 curl -w "\n" -X GET http://localhost:7080/user/v1/health
--- wiremock/start.sh
+++ wiremock/start.sh
@@ -2,14 +2,28 @@
 clear
 
 port=7080
-OPEN_UI_URLS[0]="http://localhost:7080/"
+OPEN_UI_URLS[0]="http://localhost:7080"
+
+# wiremock-standalone-2.9.0.jar
+wm_app_name=wiremock-jre8-standalone
+wm_version=2.24.1
+
+FILE=${wm_app_name}-${wm_version}.jar 
+if [ -f "$FILE" ]; then
+    echo "$FILE exist"
+else 
+    curl -O http://repo1.maven.org/maven2/com/github/tomakehurst/${wm_app_name}/${wm_version}/${wm_app_name}-${wm_version}.jar 
+    sleep 5
+fi
 
 # echo " ${arrIN[0]}     ${port}"
 printf "\n%s%s\n" "----------- $(date '+%F %T,%3N') [START:mock service] on port:${port}"
-java -jar wiremock-jre8-standalone-2.24.1.jar --port=${port} &
+java -jar ${wm_app_name}-${wm_version}.jar --port=${port} &
 printf "\n\n\n"
 
 sleep 10
+
+curl -w "\n" ${OPEN_UI_URLS[0]}/user/v1/health
 
 # open -na "Google Chrome" --args --incognito  ${OPEN_UI_URLS[0]}
 echo
