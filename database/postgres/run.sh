925fc918ae314dc444a3a18deda233b335e098b1
Krishna Manchikalapudi <krishna.manchikalapudi@gmail.com>
2020-10-23 11:56:45 -0700

    postgres working file

--- database/postgres/run.sh
+++ database/postgres/run.sh
@@ -7,19 +7,21 @@ DATE_TIME=`date '+%Y-%m-%d %H:%M:%S'`
 
 export containerName=postgres
 export hostAddress=127.0.0.1
-export hostPort=3306
+export hostPort=5432
 export WEB_ADDR="http://${hostAddress}:${hostPort}"
 export ROOT_USERNAME=root
-export ROOT_PASSWORD=my-secret-pw
+export ROOT_PASSWORD='mysecretpassword'
+current_folder=`pwd`
 
-
-printf "\n -------- Downloading container: ${containerName} -------- \n "  
+printf "\n -------- Downloading container: ${containerName} -------- \n "
 docker pull ${containerName}:latest &
 
 
 sleep 15
 printf "\n -------- Starting container: ${containerName}  -------- \n"
-docker run -d --name postgres -e POSTGRES_PASSWORD=${ROOT_PASSWORD} -v ~/TOOLS/postgres/postgres_data:/var/lib/postgresql/data ${containerName} &
+
+# docker run --name postgres -v ${pwd}:/var/lib/postgresql/data -p 5432:5432 -e POSTGRES_PASSWORD=mysecretpassword -d postgres &
+docker run --name ${containerName} -v ${pwd}/scripts:/var/lib/postgresql/data -p ${hostPort}:${hostPort} -e POSTGRES_PASSWORD=mysecretpassword -d ${containerName}  &
 
 sleep 15
 
@@ -27,7 +29,7 @@ printf '\n\n -------- Container information -------- \n'
 printf "\n\n%s\n" " -------- Container information -------- "
 containerId=$(docker container ls -a | grep ${containerName} | awk '{print $1}')
 #members=$(docker exec -t ${containerName} members)
-processId=$(lsof -nP -iTCP:${hostPort}); 
+processId=$(lsof -nP -iTCP:${hostPort});
 #processId=`lsof -nP -iTCP:5984`
 printf "\n%s\n" " Current DT: $DATE_TIME"
 printf "\n%s\n" " Container name: ${containerName}"
@@ -37,6 +39,7 @@ printf "\n\n"
 
 
 sleep 2
+# docker logs -f 8ff79dcb93adf973d4b31d2acf2347c77fe5c5137093d732d3199e51e2a57415  &
 docker logs -f $containerId &
 
 sleep 15
