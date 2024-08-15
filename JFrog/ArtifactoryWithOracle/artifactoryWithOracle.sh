373dd14b80747ee7c0148cee07680c425b803077
Krishna Manchikalapudi <krishnam@jfrog.com>
2024-08-14 19:14:53 -0700

    Oracle database for Artifactory

---
+++ JFrog/ArtifactoryWithOracle/artifactoryWithOracle.sh
@@ -0,0 +1,120 @@
+#!/bin/bash
+arg=${1}
+DATE_TIME=`date '+%Y-%m-%d %H:%M:%S'`
+
+NAMESPACE="artifactoryWithOracle"
+alias k=kubectl
+
+
+prestep() {
+    helm repo add oracle https://oracle.github.io/helm-charts
+    helm repo add jfrog https://charts.jfrog.io
+}
+oracle-install(){
+    printf "\n ----------------------------------------------------------------  "
+    printf "\n ------------ INSTALLING... Oracle database on K8S ------------  "
+    printf "\n ----------------------------------------------------------------  \n"
+
+    # check for oracle image folder, clone or update repo
+    rm -rf oracle-db-1.0.0.tgz
+    FOLDER=docker-images
+    if [ ! -d "$FOLDER" ] ; then
+        git clone https://github.com/oracle/docker-images.git
+    else
+        cd "$FOLDER"
+        git pull https://github.com/oracle/docker-images.git
+    fi
+
+    #git clone https://github.com/oracle/docker-images.git
+    chmod -R 755 *
+    helm package docker-images/OracleDatabase/SingleInstance/helm-charts/oracle-db
+
+    helm install db19c --set oracle_sid=ORCL,oracle_pdb=prod oracle-db-1.0.0.tgz
+}
+oracle-delete() {
+    rm -rf docker-images
+    rm -rf oracle-db-1.0.0.tgz
+}
+
+artifactoy-install() {
+    printf "\n ----------------------------------------------------------------  "
+    printf "\n ------------ INSTALLING... JFrog Artifactory on K8S ------------  "
+    printf "\n ----------------------------------------------------------------  \n"
+    export MASTER_KEY=$(openssl rand -hex 32) && echo "MASTER KEY: ${MASTER_KEY} \n"
+
+    export JOIN_KEY=$(openssl rand -hex 32) && echo "Join KEY: ${JOIN_KEY} \n"
+
+    kubectl create ns ${NAMESPACE} 
+    # Create a secret containing the key. The key in the secret must be named master-key
+    kubectl create secret generic my-masterkey-secret -n ${NAMESPACE} --from-literal=master-key=${MASTER_KEY}
+    kubectl create secret generic my-joinkey-secret -n ${NAMESPACE} --from-literal=join-key=${JOIN_KEY}
+
+    # Install the chart with the release name  artifactory and with master key and join key.
+    helm upgrade --install artifactory --set artifactory.replicaCount=1 --set artifactory.masterKey=${MASTER_KEY} --set artifactory.joinKey=${JOIN_KEY} --namespace ${NAMESPACE} jfrog/artifactory
+    # kubectl scale svc/artifactory -n artifactory --current-replicas=2 --replicas=1 
+
+    sleep 30
+
+    # expose artifactory as NodePort
+    export LOCAL_IP=$(ipconfig getifaddr en0)
+    kubectl patch svc artifactory-artifactory-nginx -n ${NAMESPACE} -p '{"spec": {"type": "NodePort"}}'
+    sleep 5
+
+    # expose postgres as NodePort
+    kubectl patch svc artifactory-postgresql -n ${NAMESPACE} -p '{"spec": {"type": "NodePort"}}'
+    sleep 5
+    
+    # Change default password ref: https://jfrog.com/help/r/jfrog-rest-apis/change-password
+}
+artifactoy-serviceInfo() {
+    printf "\n ----------------------------------------------------------------  "
+    printf "\n ----------------  JFrog Artifactory: K8S Info  ----------------  "
+    printf "\n ----------------------------------------------------------------  \n"
+}
+artifactoy-delete(){
+    printf "\n ----------------------------------------------------------------  "
+    printf "\n ------------ CLEANING the JFrog Artifactory on K8S ------------  "
+    printf "\n ----------------------------------------------------------------  \n"
+    helm uninstall ${NAMESPACE} && sleep 90 && kubectl delete pvc -l app=artifactory
+    kubectl delete ns ${NAMESPACE} --force=true --ignore-not-found=true
+    printf "\n CLEANING: COMPLETE at $(date +"%Y-%m-%d %H:%M:%S") \n"
+}
+
+
+# Check for 1 argument
+if [ $# -ne 1 ]; then
+  echo "Error: This script requires exactly 1 arguments."
+  echo "    ./artifactory.sh <install | info | delete> "
+fi
+# -z option with $1, if the first argument is NULL. Set to default
+if  [[ -z "$1" ]] ; then # check for null
+    echo "User action is NULL, setting to default INSTALL"
+    arg='INSTALL'
+fi
+
+# -n string - True if the string length is non-zero.
+if [[ -n $arg ]] ; then
+    arg_len=${#arg}
+    # uppercase the argument
+    arg=$(echo ${arg} | tr [a-z] [A-Z] | xargs)
+    echo "User Action: ${arg}, and arg length: ${arg_len}"
+    
+    if [[ "INSTALL" == "${arg}" ]] ; then   # Download & install 
+        artifactoy-install
+        sleep 5
+        artifactoy-serviceInfo
+    elif [[ "DELETE" == "${arg}" ]] ; then   # delete 
+        artifactoy-delete
+    elif [[ "INFO" == "${arg}" ]] ; then   # Info 
+        artifactoy-serviceInfo
+    elif [[ "PRESTEP" == "${arg}" ]] ; then   # Info 
+        prestep
+    elif [[ "DBINSTALL" == "${arg}" ]] || [[ "DB-INSTALL" == "${arg}" ]] ; then   # Info 
+        oracle-install
+    fi
+fi
+
+printf "\n ----------------------------------------------------------------  "
+
+printf "\n***** [END] TS: $(date +"%Y-%m-%d %H:%M:%S") \n\n"
+
